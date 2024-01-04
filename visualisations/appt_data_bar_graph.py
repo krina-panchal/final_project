abe00215f066410318775a20085d8c71c9b749ea
Krina <krinapanchal04@gmail.com>
2024-01-04 12:28:58 +0000

    updating bar graph

--- visualisations/appt_data_bar_graph.py
+++ visualisations/appt_data_bar_graph.py
@@ -1,7 +1,6 @@
 # Importing necessary data analysis and visualisation Python libraries
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Loading dataset using Pandas
 # Note: Update the file path based on the location of the attp_data.xlsx file
@@ -9,20 +8,35 @@ appt_data = pd.read_excel(r"C:\Users\krina\OneDrive - Queen Mary, University of 
 
 # Data Visualisation
 
-#Bar chart of the count of applicants by Ethnicity Group
-# Creating a new figure with a specified size
+# Bar chart of the count of applicants by Ethnicity Group
 plt.figure(figsize=(20, 10))
 
-# Use Seaborn to create a count plot
-sns.countplot(x='Ethnic Group', data=appt_data, hue='Gender', palette={'Male': 'lightblue', 'Female': 'pink'})
+# Getting unique values for the 'Gender' column
+genders = appt_data['Gender'].unique()
+
+# Creating bars for each gender
+bar_width = 0.35  # Adjust this value as needed
+for i, gender in enumerate(genders):
+    gender_data = appt_data[appt_data['Gender'] == gender]
+    ethnic_group_counts = gender_data['Ethnic Group'].value_counts()
+    
+    # Plotting each bar
+    plt.bar(
+        x=range(len(ethnic_group_counts)) if i == 0 else [pos + bar_width for pos in range(len(ethnic_group_counts))],
+        height=ethnic_group_counts,
+        width=bar_width,
+        label=gender,
+        color='lightblue' if gender == 'Male' else 'pink'
+    )
 
 # Customising the plot labels and title
 plt.xlabel('Ethnicity Group')
 plt.ylabel('Count')
 plt.title('Distribution of Applicants by Ethnicity Group and Gender')
 
-# Add legend to show the hue (Gender)
-plt.legend()
+# Adding x-axis labels for each ethnic group
+plt.xticks(ticks=[pos + bar_width / 2 for pos in range(len(ethnic_group_counts))], labels=ethnic_group_counts.index)
 
-# Displays the plot
+plt.legend()
 plt.show()
+
