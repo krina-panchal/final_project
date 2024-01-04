7d66f6401a0e47eadb590bf3447e23135f33c56e
Krina <krinapanchal04@gmail.com>
2024-01-04 15:17:43 +0000

    load geopandas shapefile

--- visualisations/ucas_data_piechart.py
+++ visualisations/ucas_data_piechart.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load dataset
 ucas_data = pd.read_excel(r"C:\Users\krina\OneDrive - Queen Mary, University of London\Desktop\Year2\DAT5902\final_project\visualisations\ucas_data.xlsx")
